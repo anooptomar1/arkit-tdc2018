6f07aec2e5395464fb32000c86a5398ebce79236
Vitor Navarro <vitor.navarro87@gmail.com>
2018-07-08 14:29:34 -0300

    STEP1: Config and Box

--- PreTDCAR/ViewController.swift
+++ PreTDCAR/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var arSceneView: ARSCNView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if ARWorldTrackingConfiguration.isSupported {
+            configureARKit()
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        arSceneView.session.pause()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func configureARKit() {
+        let worldConfiguration = ARWorldTrackingConfiguration()
+        if #available(iOS 11.3, *) {
+            worldConfiguration.planeDetection = [.horizontal, .vertical]
+        } else {
+            worldConfiguration.planeDetection = [.horizontal]
+        }
+        arSceneView.session.run(worldConfiguration, options: [.resetTracking, .removeExistingAnchors])
     }
-
-
+    
+    func addCube(parent: SCNNode) {
+        // As medidas são em metros, então construímos um cubo de 0.2m == 20cm
+        let cubeGeometry: SCNBox = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0.0)
+        // Aplicamos uma cor ao material difuso padrão
+        cubeGeometry.firstMaterial?.diffuse.contents = UIColor.orange
+        let cubeNode = SCNNode(geometry: cubeGeometry)
+        cubeNode.position = SCNVector3(x: 0.0, y: 0.0, z: 0.0)
+        parent.addChildNode(cubeNode)
+    }
+    
+    @IBAction func onAddObjectTouched() {
+        //Adicionamos o cubo ao centro do parent
+        addCube(parent: arSceneView.scene.rootNode)
+    }
+    
 }
 
